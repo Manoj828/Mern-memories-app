1534896e133dd6bff62f7d04042d8465966fb1b5
Manoj <manojanand828@gmail.com>
2021-04-06 23:24:18 +0530

    Mern app

---
+++ server/routes/posts.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+
+export default router;
+
+/*
+import axios from 'axios';
+
+const url = 'http://localhost:5000/posts';
+
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
+
+*/
+
